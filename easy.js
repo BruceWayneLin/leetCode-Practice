6a919ee6c33352938f6a91190de2841e510c3647
skanda <anikiwayne@gmail.com>
2022-03-29 00:22:04 +0800

    reverse string

--- easy.js
+++ easy.js
@@ -1,4 +1,5 @@
 module.exports = class easy {
+    //1. question two sum 
     calculateTwoSum() {
         // two sums
         const twoSum = (nums, target) => {
@@ -20,4 +21,28 @@ module.exports = class easy {
         };
         console.log(twoSum([1, 3, 5, 2, 4], 9));
     }
+
+    //2. question reverse string 
+    // Write a function that takes a string as input and returns the string reversed.
+    // Example: Given s = "hello", return "olleh".
+    reverseString(s) {
+        s = 'helloplayer';
+        var result = "";
+        var ary = s.split("");
+        for(var i = 0, max = (ary.length-1)/2 ; i < max ; i++){
+            var temp = ary[i];
+            ary[i] = ary[ary.length - 1 - i];
+            ary[ary.length - 1 - i] = temp;
+
+        }
+        return ary.join("");
+        // s = 'hello'
+        // var result = ""; 
+        // var ary = s.split(""); 
+        // for(var i = ary.length-1 ; i >= 0 ; i--){
+        //     result = result + ary[i]; 
+        // }
+        // console.log(result);
+        // return result;
+    };
 }
--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const easyList = require('./easy');
 const easy = new easyList();
 
 
-easy.calculateTwoSum();
+easy.reverseString();
